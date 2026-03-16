3e7cc5fb529ff35ee5b37c6d2c8c5f657bfe0a49
Nikolaev Ivan <akovalev.ren.088@gmail.com>
2026-03-16 22:22:52 +0300

    update model

--- internal/model/study.go
+++ internal/model/study.go
@@ -7,11 +7,10 @@ type StudyProgress struct{
     UserID       string    `db:"user_id"`
     QuestionID       string    `db:"question_id"`
 
-    EasyFactor     float64    `db:"easy_factor"`
-    Interval     int64    `db:"interval"`
-    Repetitions     int64    `db:"repetitions"`
+    EaseFactor     float64    `db:"ease_factor"`
+    Interval     int32    `db:"interval"`
+    Repetitions     int32    `db:"repetitions"`
 
     NextReviewAt    time.Time `db:"next_review_at"`
     LastReviewAt    time.Time `db:"last_reviewed_at"`
-    CreatedAt    time.Time `db:"created_at"`
 }
