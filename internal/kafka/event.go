9f71ec56e14417e8fa39e7c07bb08b364cf8ebcc
Nikolaev Ivan <akovalev.ren.088@gmail.com>
2026-03-21 21:37:19 +0300

    feature: realize kafka producer

--- internal/kafka/event.go
+++ internal/kafka/event.go
@@ -3,10 +3,10 @@ package kafka
 import "time"
 
 type StudyEvent struct{
-	UserID int64 `json:"user_id"`
-	QuestionID int64 `json:"question_id"`
-	Quality int64 `json:"quality"`
-	EaseFactor int64 `json:"ease_factor"`
+	UserID string `json:"user_id"`
+	QuestionID string `json:"question_id"`
+	Quality int32 `json:"quality"`
+	EaseFactor float64 `json:"ease_factor"`
 	Interval int64 `json:"interval"`
 	Timestamp time.Time `json:"timestamp"`
 }
--- internal/kafka/producer.go
+++ internal/kafka/producer.go
@@ -1,1 +1,69 @@
 package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"google.golang.org/genproto/googleapis/maps/fleetengine/delivery/v1"
+)
+
+type Producer struct{
+	producer *kafka.Producer
+	topic string
+}
+
+func NewProducer(brokers, topic string) (*Producer, error){
+	p, err := kafka.NewProducer(
+		&kafka.ConfigMap{
+			"bootsrap.servers": brokers,
+		},
+	)
+
+	if err != nil{
+		return nil, fmt.Errorf("Kafka new producer error: %w", err)
+	}
+
+	return &Producer{
+		producer: p,
+		topic: topic,
+	}, nil
+
+}
+
+
+func (p *Producer) Send(ctx context.Context, key string, event any) error{
+	value, err := json.Marshal(event)
+	
+	if err != nil{
+		return fmt.Errorf("Marshal event error: %w", err)
+	}
+
+	msg := &kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic: &p.topic,
+			Partition: kafka.PartitionAny,
+		},
+		Key: []byte(key),
+		Value: value,
+	}
+
+	deliveryChan := make(chan kafka.Event, 1)
+	if err := p.producer.Produce(msg, deliveryChan); err != nil{
+		return fmt.Errorf("produce error: %w", err)
+	}
+
+	e := <-deliveryChan
+	m := e.(*kafka.Message)
+	if m.TopicPartition.Error != nil{
+		return fmt.Errorf("delivery failed: %w", m.TopicPartition.Error)
+	}
+
+	return nil
+}
+
+func (p *Producer) Close(){
+	p.producer.Flush(5000)
+	p.producer.Close()
+}
